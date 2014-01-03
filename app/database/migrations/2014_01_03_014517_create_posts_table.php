2bdfcefb2a0c4d87af4fc8e4e5bb48e7603d5caa
Zizaco <zizaco@gmail.com>
2014-01-03 00:21:53 -0200

    Added 'create_posts_table' migration in order to have a table to store blog posts.

---
+++ app/database/migrations/2014_01_03_014517_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('lean_content')->nullable();
+            $table->text('content');
+            $table->integer('author_id'); // users table relation
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+
+}
