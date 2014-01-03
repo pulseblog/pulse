7b1c6953b7469c45356707ae0be684d0c7b98896
Zizaco <zizaco@gmail.com>
2014-01-03 02:06:40 -0200

    Added 'Pulse\Cms\PageRepository::findBySlug' method in order to find pages by slug

--- app/models/Pulse/Cms/PageRepository.php
+++ app/models/Pulse/Cms/PageRepository.php
@@ -5,12 +5,19 @@ use App;
 /**
  * Class PageRepository
  *
- * The Pulse\Cms\PageRepository abstracts all the database queries regarding Pages.
+ * The Pulse\Cms\PageRepository abstracts all the database queries regarding
+ * Pages.
  *
  * @package  Pulse\Cms
  */
 class PageRepository
 {
+    /**
+     * The domain object class that is going to query for using
+     * Eloquent methods.
+     * @var string
+     */
+    public $domainObject = 'Pulse\Cms\Page';
 
     /**
      * Finds a Page by the given id
@@ -19,8 +26,20 @@ class PageRepository
      */
     public function find($id)
     {
-        $page = App::make('Pulse\Cms\Page');
+        $page = App::make($this->domainObject);
 
         return $page::find($id);
+    }
+
+    /**
+     * Finds a Page by the given slug
+     * @param  string $slug Page slug
+     * @return Page A Page object or null.
+     */
+    public function findBySlug($slug)
+    {
+        $page = App::make($this->domainObject);
+
+        return $page::where('slug', $slug)->first();
     }
 }
--- app/tests/models/Pulse/Cms/PageRepositoryTest.php
+++ app/tests/models/Pulse/Cms/PageRepositoryTest.php
@@ -18,10 +18,34 @@ class PageRepositoryTest extends TestCase
         $page = m::mock('Pulse\Cms\Page');
 
         // Expectations
-        $page->shouldReceive('find')->once()->andReturn(m::self());
+        $page->shouldReceive('find')
+            ->once()->with(1)
+            ->andReturn(m::self());
+
         App::instance('Pulse\Cms\Page', $page);
 
         // Assertion
         $this->assertEquals($page, $repo->find(1));
+    }
+
+    public function testShouldFindBySlug()
+    {
+        // Set
+        $repo = new PageRepository;
+        $page = m::mock('Pulse\Cms\Page');
+        $slug = 'the_page_slug';
+
+        // Expectations
+        $page->shouldReceive('where')
+            ->once()->with('slug',$slug)
+            ->andReturn(m::self());
+
+        $page->shouldReceive('first')
+            ->once()->andReturn(m::self());
+
+        App::instance('Pulse\Cms\Page', $page);
+
+        // Assertion
+        $this->assertEquals($page, $repo->findBySlug($slug));
     }
 }
