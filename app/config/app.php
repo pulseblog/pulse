2f702397138347771482228e2040a69581567843
Zizaco <zizaco@gmail.com>
2014-01-10 03:03:49 -0200

    Added Confide to the project in order to handle user accounts.

--- app/config/app.php
+++ app/config/app.php
@@ -106,6 +106,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Zizaco\Confide\ConfideServiceProvider',
 
 	),
 
@@ -172,6 +173,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Confide'         => 'Zizaco\Confide\ConfideFacade',
 
 	),
 
