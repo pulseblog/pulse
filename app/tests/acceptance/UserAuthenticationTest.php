06b87c8f690d91a6cd8ea2f2d0d4dc8dc7c2a267
Zizaco <zizaco@gmail.com>
2014-01-10 03:33:47 -0200

    Added UserAuthentication acceptance test

---
+++ app/tests/acceptance/UserAuthenticationTest.php
@@ -0,0 +1,76 @@
+<?php
+
+use Mockery as m;
+
+/**
+ * Feature: As an admin I would like to be able to login into the application
+ */
+class UserAuthenticationTest extends AcceptanceTestCase {
+
+    /**
+     * Scenario: Login into the application
+     * @return void
+     */
+    public function testShouldLogin()
+    {
+        // Given
+        $this->site_has_user([
+            'username' => 'Someone',
+            'email' => 'someone@somewhere.com',
+            'password' => 'foobarl337'
+        ]);
+
+        // Then
+        $this->i_expect_to_become_logged_as([
+            'email' => 'someone@somewhere.com',
+            'password' => 'foobarl337'
+        ]);
+
+        // When
+        $this->i_visit_url('user/login');
+
+        // And
+        $this->i_submit_form_with('form', [
+            'email' => 'someone@somewhere.com',
+            'password' => 'foobarl337'
+        ]);
+    }
+
+    /**
+     * Create a sample user
+     * @param  array $data Attributes of the Post
+     * @return void
+     */
+    protected function site_has_user($data)
+    {
+        // Definition
+        $post = App::make('Pulse\User\User');
+
+        foreach ($data as $key => $value) {
+            $post->{$key} = $value;
+        }
+    }
+
+    protected function i_expect_to_become_logged_as($input)
+    {
+        // Definition
+        $input['_token'] = '';
+        $repo = m::mock('Pulse\User\Repository');
+
+        // Expectation
+        $repo->shouldReceive('login')
+            ->with($input)->once()
+            ->andReturn(true);
+
+        App::instance('Pulse\User\Repository', $repo);
+    }
+
+    /**
+     * Closes mockery expectations
+     * @return void
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
+}
