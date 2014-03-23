0223a6419caa0f69d26631a726c2bbf625b6709e
Guilherme Guitte <guilherme.guitte@gmail.com>
2014-03-23 10:57:08 -0300

    added translate to Post resource attributes

--- app/lang/en/resources.php
+++ app/lang/en/resources.php
@@ -7,12 +7,20 @@ return array(
 
     'attributes' => array(
         'Page' => array(
-            'title' => 'Title',
-            'slug' => 'Slug',
+            'title'        => 'Title',
+            'slug'         => 'Slug',
             'lean_content' => 'Short Description',
-            'content' => 'Content',
-            'author' => 'Author',
-            'created_at' => 'Created at',
+            'content'      => 'Content',
+            'author'       => 'Author',
+            'created_at'   => 'Created at',
+        ),
+        'Post' => array(
+            'title'        => 'Title',
+            'slug'         => 'Slug',
+            'lean_content' => 'Short Description',
+            'content'      => 'Content',
+            'author'       => 'Author',
+            'created_at'   => 'Created at',
         )
     )
 );
