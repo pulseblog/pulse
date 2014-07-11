9861d4332210016f3bb9dd245da78773191d58ce
Zizaco <zizaco@gmail.com>
2014-07-10 22:31:50 -0300

    Updated front-end stylesheets and markup

--- app/views/front/posts/index.blade.php
+++ app/views/front/posts/index.blade.php
@@ -2,24 +2,22 @@
 
 @section('content')
     @foreach ($posts as $post)
-        <div class="l-block-1">
-            {{ link_to_action('Pulse\Frontend\CmsController@showPost', $post->title, ['slug'=>$post->slug], ['class'=>'title']) }}
+        <div class="l-block-1 postcard">
+            {{ link_to_action('Pulse\Frontend\CmsController@showPost', $post->title, ['slug'=>$post->slug], ['class'=>'title h3']) }}
 
-            <div class="published-at">
-                {{ date('M D, Y', strtotime($post->created_at)) }}
-            </div>
+            <span class="date">
+                {{ date('M d, Y', strtotime($post->created_at)) }}
+            </span>
 
-            <img src="http://lorempixel.com/837/350/nature" alt="" class="m-post-image">
-
-            <div class="m-post-short-content">
+            <div class="short-content">
                 {{{ $post->lean_content }}}
             </div>
 
-            {{ link_to_action('Pulse\Frontend\CmsController@showPost', 'Read more', ['slug'=>$post->slug], ['class'=>'title']) }}
+            {{ link_to_action('Pulse\Frontend\CmsController@showPost', 'Read more', ['slug'=>$post->slug], ['class'=>'read-more']) }}
         </div>
     @endforeach
 
     @if (count($posts) == 0)
-        <div class="m-no-post-found">No posts =(</div>
+        <div class="h2">No posts =(</div>
     @endif
 @stop
