49a3ae81d4d6b534bff6aa11f152118da29ab54f
Zizaco <zizaco@gmail.com>
2014-07-12 10:45:23 -0300

    Added social_networks to pulse config file in order to customize the default template footer.

--- app/config/pulse.php
+++ app/config/pulse.php
@@ -9,6 +9,43 @@ return array(
     |
     */
 
-    'blogname'     => "Pulse Blog",
+    'blogname' => "Pulse Blog",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Discuss Name
+    |--------------------------------------------------------------------------
+    |
+    | Tells the Disqus service your forum's shortname, which is the unique
+    | identifier for your website as registered on Disqus. If null, the
+    | Disqus embed will not load.
+    |
+    */
+
     'discuss_name' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Social Networks
+    |--------------------------------------------------------------------------
+    |
+    | Social network URLs that will be displayed in the blog
+    |
+    */
+
+    'social_networks' => array(
+        'facebook'    => null,
+        'googleplus'  => null,
+        'github'      => null,
+        'twitter'     => null,
+        'deviantart'  => null,
+        'pinterest'   => null,
+        'youtube'     => null,
+        'vimeo'       => null,
+        'tumblr'      => null,
+        'flickr'      => null,
+        'instagram'   => null,
+        'lastfm'      => null,
+        'linkedin'    => null,
+    ),
 );
--- app/views/front/templates/_footer.blade.php
+++ app/views/front/templates/_footer.blade.php
@@ -1,9 +1,42 @@
 <div class="l-block-1 social-links">
     <hr>
-    <a href="#" title="Google Plus" class="social-icon">&#xe039;</a> {{-- Google Plus --}}
-    <a href="#" title="Github" class="social-icon">&#xe037;</a> {{-- Github --}}
-    <a href="#" title="Twitter" class="social-icon">&#xe086;</a> {{-- Twitter --}}
-    <a href="#" title="Instagram" class="social-icon">&#xe100;</a> {{-- Instagram --}}
-    <a href="#" title="Last.fm" class="social-icon">&#xe051;</a> {{-- Last.fm --}}
-    <a href="#" title="Linkedin" class="social-icon">&#xe052;</a> {{-- Linkedin --}}
+    @if (Config::get('pulse.social_networks.facebook'))
+        {{ link_to(Config::get('pulse.social_networks.facebook'), "facebook", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.googleplus'))
+        {{ link_to(Config::get('pulse.social_networks.googleplus'), "googleplus", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.github'))
+        {{ link_to(Config::get('pulse.social_networks.github'), "githubalt", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.twitter'))
+        {{ link_to(Config::get('pulse.social_networks.twitter'), "twitterbird", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.deviantart'))
+        {{ link_to(Config::get('pulse.social_networks.deviantart'), "deviantart", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.pinterest'))
+        {{ link_to(Config::get('pulse.social_networks.pinterest'), "pinterest", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.youtube'))
+        {{ link_to(Config::get('pulse.social_networks.youtube'), "youtube", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.vimeo'))
+        {{ link_to(Config::get('pulse.social_networks.vimeo'), "vimeo", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.tumblr'))
+        {{ link_to(Config::get('pulse.social_networks.tumblr'), "tumblr", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.flickr'))
+        {{ link_to(Config::get('pulse.social_networks.flickr'), "flickr", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.instagram'))
+        {{ link_to(Config::get('pulse.social_networks.instagram'), "instagram", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.lastfm'))
+        {{ link_to(Config::get('pulse.social_networks.lastfm'), "lastfm", ['class'=>'social-icon']) }}
+    @endif
+    @if (Config::get('pulse.social_networks.linkedin'))
+        {{ link_to(Config::get('pulse.social_networks.linkedin'), "linkedin", ['class'=>'social-icon']) }}
+    @endif
 </div>
